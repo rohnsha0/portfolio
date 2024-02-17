32314035d2b62d241667d4158941cc8b6342c726
Rohan Shaw <rohnsha0@gmail.com>
2024-02-17 19:55:12 +0530

    stocksense: updated favicon, stocksense

--- pages/stocksense.py
+++ pages/stocksense.py
@@ -3,11 +3,14 @@ import os
 
 st.set_page_config(
     page_title="Rohan Shaw: StockSense", 
-    page_icon=os.path.join("assets", "swasthai-favicon.png"),
+    page_icon=os.path.join("assets", "stocksense-favicon.png"),
     layout="centered", 
     initial_sidebar_state="collapsed"
 )
 
+#hide the hamburger menu
+hide_decoration_bar_style = '''<style>header {visibility: hidden;}</style>'''
+st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
 
 with open(
     os.path.join("assets", "styles.css")
@@ -22,6 +25,9 @@ st.write("""
 st.link_button("View on Github", url="https://github.com/rohnsha0/stocksense", type="primary")
 
 st.header("AI-based Stock Price Prediction")
-st.write("""
-    The flagship feature of the project where users can search for their preferred stocks to get a detailed idea for the future stock price. 
-""")
+st.write("""The flagship feature of the project where users can search for their preferred stocks to get a detailed 
+idea for the future stock price using sophisticated LSTM neural network. It is based upon temporal data that updated 
+frequently. """)
+with st.expander("Get a hands-on experience on the stock price prediction"):
+    st.error("Feature unavailable")
+st.subheader("How it works?")
