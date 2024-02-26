81d896e3074a2e2ca6b2abbf08519b5cf82f79ed
Rohan Shaw <rohnsha0@gmail.com>
2024-02-26 13:47:00 +0530

    swasthai: restructured and added table of contents

--- pages/swasthai.py
+++ pages/swasthai.py
@@ -25,13 +25,19 @@ with open(
 st.image(os.path.join("assets", "swasthai", "swasthai_banner.png"))
 
 with st.sidebar:
-    st.title("Select Function")
-    st.write("""
-        Used to determine what function to be implemented among various options to choose from.
-    """)
-    funcs = ["SwasthAI Home", "Documentations", "Frequently Asked Questions"]
-    fun_selection = st.selectbox("Select app function", funcs, index=0, placeholder="Unselected",
-                                 help="Used for selecting the model varient to be used for scanning the image.")
+    st.subheader("Contents in the page")
+    st.write("Click on the links below to navigate to the respective section.")
+    st.write(
+        """
+        - [About SwasthAI](#about-swasthai)
+        - [Features](#features)
+            - [Scan](#scan)
+            - [Care+](#care)
+            - [mAI](#mai)
+            - [Community](#community)
+        - [How it works?](#how-it-works)
+        """
+    )
 
 st.title('About SwasthAI')
 with st.container(border=True):
@@ -54,7 +60,10 @@ st.link_button(label="View on Github", type="primary", url="https://github.com/r
 selected_option = ""
 sub_selected_option = ""
 
-st.header("Scan")
+st.header("Features")
+st.write("Info about the features of the app.")
+
+st.subheader("Scan")
 st.write("""
     The flagship feature of the project where users can upload photocopy of their recent scan i.e. xray, mri, skin, etc to get 
     a ai-based prediction about what might gone wrong with the body part. 
@@ -99,17 +108,10 @@ with st.expander("Get an hands-on experience on how the scanning and disease pre
             st.subheader("Results")
             st.write("Results will be displayed here.")
 
-st.subheader("How it works?")
-st.write("""
-    Your medical images like X-rays and MRIs are fed into a special kind of AI called a "Convolutional Neural Network" (CNN). Imagine this network as a web of interconnected neurons, much like the human brain. But instead of thoughts, these neurons process visual information. 
-    \nHere's the magic: these neurons have been trained on massive datasets of labeled medical images, learning to recognize patterns linked to specific diseases. Just like doctors identify pneumonia in X-rays, our CNNs do the same, automatically extracting those crucial clues. 
-    \nThink of it this way: when you feed in a new image, the CNN analyzes it, calculates the probability of each possible disease, and presents it like a scorecard. It doesn't diagnose, but helps doctors see potential issues they might miss.
-""")
-
 st.warning(
     'All features below are only exclusive on the **[mobile application](https://github.com/rohnsha0/SwasthAI-androidApp)**.')
 
-st.header("Care+")
+st.subheader("Care+")
 st.write("""
     It has a bunch of smaller but useful features included into the app that are designed to help users in their day-to-day life. Some of which are included below:\n
     - Get suggested health recommendations based on your recent scan, our knowledge base about you, and your past medical history.
@@ -118,7 +120,7 @@ st.write("""
     Apart from these, there are a lot of other features included in the app that are designed to help users in their day-to-day life like contents aimed to educate, spread awareness, etc.
 """)
 
-st.header("mAI")
+st.subheader("mAI")
 st.write("""
     Our app's Chatbot feature serves as a convenient and accessible resource for users to inquire about health-related queries or obtain information about their recent scans. With natural language processing capabilities, users can seamlessly interact with the Chatbot by typing or speaking their questions, receiving prompt and accurate responses. 
     \nWhether seeking advice on symptoms, medication, or general health concerns, or wanting to understand the results of recent scans or medical tests, users can rely on the Chatbot to provide relevant information and guidance. 
@@ -128,8 +130,15 @@ with st.expander("How does it help?"):
 st.write(
     "The Chatbot enhances user experience by offering personalized assistance, facilitating informed decision-making, and empowering users to take proactive steps towards managing their health effectively within the app's ecosystem.")
 
-st.header("Community")
+st.subheader("Community")
 st.write("""
     Our engaging community provides users with a platform to post medical-related queries and receive responses from fellow community members. Users can easily compose their queries, categorize them for better organization, and submit them for feedback. 
     \nThrough engaging with queries by offering responses, upvoting helpful comments, and flagging inappropriate content, users foster a supportive environment where they can seek advice, share experiences, and offer assistance to one another regarding health-related concerns.
+""")
+
+st.header("How it works?")
+st.write("""
+    Your medical images like X-rays and MRIs are fed into a special kind of AI called a "Convolutional Neural Network" (CNN). Imagine this network as a web of interconnected neurons, much like the human brain. But instead of thoughts, these neurons process visual information. 
+    \nHere's the magic: these neurons have been trained on massive datasets of labeled medical images, learning to recognize patterns linked to specific diseases. Just like doctors identify pneumonia in X-rays, our CNNs do the same, automatically extracting those crucial clues. 
+    \nThink of it this way: when you feed in a new image, the CNN analyzes it, calculates the probability of each possible disease, and presents it like a scorecard. It doesn't diagnose, but helps doctors see potential issues they might miss.
 """)
